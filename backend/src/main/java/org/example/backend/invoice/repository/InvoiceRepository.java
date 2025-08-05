84dbb385e581087ad518dc3862eec233bb3315fa
MatthewCroft <mc8737@ship.edu>
2025-08-05 18:54:47 -0400

    Add invoice endpoints

---
+++ backend/src/main/java/org/example/backend/invoice/repository/InvoiceRepository.java
@@ -0,0 +1,11 @@
+package org.example.backend.invoice.repository;
+
+import org.example.backend.invoice.entity.InvoiceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface InvoiceRepository extends JpaRepository<InvoiceEntity, UUID> {
+    Optional<InvoiceEntity> findByIdAndUserIdAndCustomerId(UUID id, UUID userId, UUID customerId);
+}
