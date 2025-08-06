d125ceeba92e71b7e688e21005cafa293f3625f1
Matthew <matthewpcroft1@gmail.com>
2025-08-06 19:33:21 -0400

    fixing tests

--- backend/src/test/java/org/example/backend/invoice/repository/InvoiceRepositoryTest.java
+++ backend/src/test/java/org/example/backend/invoice/repository/InvoiceRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,10 +25,16 @@ class InvoiceRepositoryTest {
         InvoiceEntity e1 = new InvoiceEntity();
         e1.setUserId(userId);
         e1.setCustomerId(customerId);
+        e1.setInvoiceNumber("INV-11");
+        e1.setIssueDate(LocalDate.now());
+        e1.setStatus("UNPAID");
         invoiceRepository.save(e1);
         InvoiceEntity e2 = new InvoiceEntity();
         e2.setUserId(userId);
         e2.setCustomerId(customerId);
+        e2.setInvoiceNumber("INV-12");
+        e2.setIssueDate(LocalDate.now());
+        e2.setStatus("UNPAID");
         invoiceRepository.save(e2);
 
         List<InvoiceEntity> result = invoiceRepository.findAllByUserIdAndCustomerId(userId, customerId);
@@ -41,6 +48,9 @@ class InvoiceRepositoryTest {
         InvoiceEntity e = new InvoiceEntity();
         e.setUserId(userId);
         e.setCustomerId(customerId);
+        e.setInvoiceNumber("INV-1");
+        e.setIssueDate(LocalDate.now());
+        e.setStatus("UNPAID");
         e = invoiceRepository.save(e);
 
         assertTrue(invoiceRepository.findByIdAndUserIdAndCustomerId(e.getId(), userId, customerId).isPresent());
